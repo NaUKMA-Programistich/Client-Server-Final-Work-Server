683429f142ed8fdf321503464fb8a894a6bd55db
Zahryvyi <olegzagraf@gmail.com>
2022-07-12 20:35:01 +0300

    log calls + server start info

--- src/main/java/org/example/controller/EndpointHandler.kt
+++ src/main/java/org/example/controller/EndpointHandler.kt
@@ -1,12 +1,16 @@
 package org.example.controller
 
 import com.sun.net.httpserver.HttpsExchange
+import com.sun.tools.javac.Main
+import java.util.logging.Logger
 
 data class EndpointHandler(
     val pattern: String,
     val method: String,
     val handler: (HttpsExchange) -> Unit
 ) {
+    private val log = Logger.getLogger(Main::class.java.name)
+
     fun isMatch(exchange: HttpsExchange): Boolean {
         if (!exchange.requestMethod.equals(method)) return false
         val path: String = exchange.requestURI.path
@@ -15,6 +19,7 @@ data class EndpointHandler(
 
     fun handle(exchange: HttpsExchange) {
         exchange.responseHeaders["Content-Type"] = "application/json"
+        log.info("URI: " + exchange.requestURI)
         handler.invoke(exchange)
     }
 }
